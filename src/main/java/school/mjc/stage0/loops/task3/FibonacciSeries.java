46dde484dd1709c61b3ee206d0df7fe422a1eae5
grigoriev-2596 <grigoriev.egor2596@gmail.com>
2022-10-31 15:28:25 +0300

    all tasks are done

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int prevValue = 0;
+        int nextValue = 1;
+        int temp;
+        for(int i = 0; i < lastFibonacci; i++) {
+            System.out.println(prevValue);
+            temp = nextValue + prevValue;
+            prevValue = nextValue;
+            nextValue = temp;
+        }
     }
 }
